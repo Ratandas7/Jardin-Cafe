0e79a42530a7121af4ea1202ffa786f0abb064fb
Ratandas7 <ratandas7720@gmail.com>
2025-04-09 09:33:26 +0600

    mobile menu responsive

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navBar = document.querySelector(".menu");
+    navBar.classList.toggle("active");
+}
+
+
+//****** new menu code
+
+/* hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+} */
+
+//****** new menu end code
